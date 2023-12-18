ce7e8ba8e11411940f46d2524b3b7c4f4bc26642
Shakeeb Ahmed <me@shakeebahmed.com>
2023-12-18 12:37:27 +0300

    Extend plugin bootstrap with Loader

--- includes/class-plugin.php
+++ includes/class-plugin.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Plugin functionality.
+ * The plugin bootstrap.
  *
  * @package AdvancedAds\Genesis
  * @author  Advanced Ads <info@wpadvancedads.com>
@@ -9,7 +9,7 @@
 
 namespace AdvancedAds\Genesis;
 
-use AdvancedAds\Framework\Interfaces\Integration_Interface;
+use AdvancedAds\Framework\Loader;
 
 defined( 'ABSPATH' ) || exit;
 
@@ -18,7 +18,7 @@ defined( 'ABSPATH' ) || exit;
  *
  * @since 1.0.0
  */
-class Plugin implements Integration_Interface {
+class Plugin extends Loader {
 
 	/**
 	 * Main instance
@@ -27,81 +27,92 @@ class Plugin implements Integration_Interface {
 	 *
 	 * @return Plugin
 	 */
-	public static function get() {
+	public static function get(): Plugin {
 		static $instance;
 
 		if ( null === $instance ) {
 			$instance = new Plugin();
+			$instance->setup();
 		}
 
 		return $instance;
 	}
 
 	/**
-	 * Hook into WordPress.
+	 * Get plugin version
+	 *
+	 * @return string
 	 */
-	public function hooks() {
-		$this->init();
-		$this->load_plugin_textdomain();
+	public function get_version(): string {
+		return AAG_VERSION;
 	}
 
 	/**
-	 * Initialize plugin.
+	 * Bootstrap plugin.
 	 *
 	 * @return void
 	 */
-	public function init() {
+	private function setup(): void {
+		$this->define_constants();
+		$this->includes();
+
+		add_action( 'init', [ $this, 'load_textdomain' ] );
+		$this->load();
+	}
+
+	/**
+	 * Define Advanced Ads constant
+	 *
+	 * @return void
+	 */
+	private function define_constants(): void {
+		$this->define( 'AA_GENESIS_ABSPATH', dirname( AAG_FILE ) . '/' );
+		$this->define( 'AA_GENESIS_BASENAME', plugin_basename( AAG_FILE ) );
+		$this->define( 'AA_GENESIS_BASE_URL', plugin_dir_url( AAG_FILE ) );
+		$this->define( 'AA_GENESIS_SLUG', 'advanced-ads-genesis' );
+
+		// Deprecated Constants.
+		/**
+		 * AAG_BASE_PATH
+		 *
+		 * @deprecated 1.1.0 use AA_GENESIS_ABSPATH now.
+		 */
+		define( 'AAG_BASE_PATH', AA_GENESIS_ABSPATH );
+
+		/**
+		 * AAG_BASE_URL
+		 *
+		 * @deprecated 1.1.0 use AA_GENESIS_BASE_URL now.
+		 */
+		define( 'AAG_BASE_URL', AA_GENESIS_BASE_URL );
+	}
+
+	/**
+	 * Includes core files used in admin and on the frontend.
+	 *
+	 * @return void
+	 */
+	private function includes(): void {
 		if ( is_admin() && ( ! defined( 'DOING_AJAX' ) || ! DOING_AJAX ) ) {
-			( new Admin() )->hooks();
+			$this->register_integration( Admin::class );
 		} else {
-			( new Frontend() )->hooks();
+			$this->register_integration( Frontend::class );
 		}
 	}
 
 	/**
-	 * Load Localisation files
-	 *
-	 * Note: the first-loaded translation file overrides any following ones if the same translation is present.
-	 *
-	 * Locales found in:
-	 *      - WP_LANG_DIR/advanced-ads-genesis/advanced-ads-genesis-LOCALE.mo
-	 *      - WP_LANG_DIR/plugins/advanced-ads-genesis-LOCALE.mo
+	 * Load the plugin text domain for translation.
 	 *
 	 * @return void
 	 */
-	public function load_plugin_textdomain() {
+	public function load_textdomain(): void {
 		$locale = determine_locale();
 		$locale = apply_filters( 'plugin_locale', $locale, 'advanced-ads-genesis' );
 
 		unload_textdomain( 'advanced-ads-genesis' );
-		load_textdomain( 'advanced-ads-genesis', WP_LANG_DIR . '/advanced-ads-genesis/advanced-ads-genesis-' . $locale . '.mo' );
-		load_plugin_textdomain( 'advanced-ads-genesis', false, AAG_BASE_DIR . '/languages' );
-	}
-
-	/**
-	 * Show notice if Advanced Ads is not activated.
-	 *
-	 * @return void
-	 */
-	public function missing_plugin_notice() {
-		$plugins = get_plugins();
-
-		$link  = wp_nonce_url( self_admin_url( 'update.php?action=install-plugin&plugin=advanced-ads' ), 'install-plugin_advanced-ads' );
-		$label = __( 'Install Now', 'advanced-ads-genesis' );
-
-		// Installed but not active.
-		if ( isset( $plugins['advanced-ads/advanced-ads.php'] ) ) {
-			$link  = wp_nonce_url( 'plugins.php?action=activate&amp;plugin=advanced-ads/advanced-ads.php&amp', 'activate-plugin_advanced-ads/advanced-ads.php' );
-			$label = __( 'Activate Now', 'advanced-ads-genesis' );
+		if ( false === load_textdomain( 'advanced-ads-genesis', WP_LANG_DIR . '/plugins/advanced-ads-genesis-' . $locale . '.mo' ) ) {
+			load_textdomain( 'advanced-ads-genesis', WP_LANG_DIR . '/advanced-ads-genesis/advanced-ads-genesis-' . $locale . '.mo' );
 		}
-		?>
-		<div class="error">
-			<p>
-				<?php echo wp_kses_post( '<strong>Genesis Ads</strong> requires the <strong><a href="https://wpadvancedads.com/#utm_source=advanced-ads&utm_medium=link&utm_campaign=activate-genesis" target="_blank">Advanced Ads</a></strong> plugin to be installed and activated on your site.', 'advanced-ads-genesis' ); ?>
-				<a class="button button-primary" href="<?php echo esc_url( $link ); ?>"><?php echo esc_html( $label ); ?></a>
-			</p>
-		</div>
-		<?php
+		load_plugin_textdomain( 'advanced-ads-genesis', false, dirname( AA_TRACKING_BASENAME ) . '/languages' );
 	}
 }
-
